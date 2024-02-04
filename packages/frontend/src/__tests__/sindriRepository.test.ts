4e95136d1066c6befb00fb63c590548f1df54473
susumu tomita <oyster880@gmail.com>
2024-02-04 23:10:06 +0900

    add repository test

---
+++ packages/frontend/src/__tests__/sindriRepository.test.ts
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import Config from 'react-native-config';
+import SindriRepository from '../repository/sindriRepository';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('SindriRepository', () => {
+  let repo: SindriRepository;
+
+  beforeEach(() => {
+    repo = new SindriRepository();
+  });
+
+  it('should make a GET request with correct endpoint and headers', async () => {
+    const endpoint = '/test';
+    await repo.getRequest(endpoint);
+
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      `${Config.SINDRI_API_URL || 'https://sindri.app/api/'}v1${endpoint}`,
+      {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${Config.SINDRI_API_KEY || ''}`,
+        },
+      }
+    );
+  });
+
+  it('should make a POST request with correct endpoint, data and headers', async () => {
+    const endpoint = '/test';
+    const data = { test: 'test' };
+    await repo.postRequest(endpoint, data);
+
+    expect(mockedAxios.post).toHaveBeenCalledWith(
+      `${Config.SINDRI_API_URL || 'https://sindri.app/api/'}v1${endpoint}`,
+      data,
+      {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${Config.SINDRI_API_KEY || ''}`,
+        },
+      }
+    );
+  });
+});
