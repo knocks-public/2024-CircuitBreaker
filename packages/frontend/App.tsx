0715089ceb791d831d9a535a30765d05903f2f64
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-02-07 00:21:38 +0900

    generate qr code including proof id

--- packages/frontend/App.tsx
+++ packages/frontend/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, TextInput, Button } from 'react-native';
+import React from 'react';
+import { Button, Keyboard, Text, TextInput, View } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 import { useGenerateProof } from './src/hooks/useGenerateProof';
 import { styles } from './src/styles/AppStyles';
 
-export default function App(): JSX.Element {
+const App = (): JSX.Element => {
   const { age, setAge, proofResult, handleGenerateProof } = useGenerateProof();
-
+  const hideKeyboard = () => {
+    Keyboard.dismiss();
+  };
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onTouchStart={hideKeyboard}>
       <TextInput
         style={styles.input}
         placeholder="Enter your age"
@@ -17,8 +20,15 @@ export default function App(): JSX.Element {
         keyboardType="numeric"
       />
       <Button title="Verify Age" onPress={handleGenerateProof} />
-      {proofResult ? <Text>{proofResult}</Text> : null}
+      {proofResult && proofResult !== '' && (
+        <View style={{ alignItems: 'center', marginTop: 20 }}>
+          <Text>Proof ID:</Text>
+          <QRCode value={proofResult} size={200} />
+        </View>
+      )}
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
