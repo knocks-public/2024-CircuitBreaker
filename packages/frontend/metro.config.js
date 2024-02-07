3e1d636d6aa092bfda9fc9c67239c5371aebc1ab
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-02-08 02:44:57 +0900

    use react-native-util as an alternative for util

--- packages/frontend/metro.config.js
+++ packages/frontend/metro.config.js
@@ -1,16 +1,32 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.resolveRequest = (context, moduleName, platform) => {
+  // @aztec/bb.js のカスタムリゾルバ
   if (moduleName === '@aztec/bb.js') {
-    const path = require('path');
     const customPath = path.resolve(
       __dirname,
       'node_modules/@aztec/bb.js/dest/node-cjs/index.js'
     );
     return {
       filePath: customPath,
+      type: 'sourceFile',
+    };
+  }
+
+  // @noir-lang/noirc_abi モジュールからの `util` の要求を react-native-util にリダイレクト
+  if (
+    context.originModulePath.includes('@noir-lang/noirc_abi') &&
+    moduleName === 'util'
+  ) {
+    const utilPath = path.resolve(
+      __dirname,
+      'node_modules/react-native-util/index.js'
+    );
+    return {
+      filePath: utilPath,
       type: 'sourceFile',
     };
   }
