2d0ba8a6995aabc6501a0cb5958a710a778a705b
susumu tomita <oyster880@gmail.com>
2024-02-08 23:12:18 +0900

    remove magic number

--- packages/frontend/src/service/SindriService.ts
+++ packages/frontend/src/service/SindriService.ts
@@ -5,7 +5,8 @@ import NoirService from './NoirService';
 class SindriService {
   private static readonly POLL_TIMEOUT_SECONDS = 20 * 60;
   private static readonly POLL_INTERVAL_MS = 1000;
-  private static readonly DEFAULT_CIRCUIT_ID = 'e98c114f-6b0d-4fe0-9379-4ee91a1c6963';
+  private static readonly DEFAULT_CIRCUIT_ID =
+    'e98c114f-6b0d-4fe0-9379-4ee91a1c6963';
   private repository: SindriRepository;
   private noirService: NoirService;
   private circuitId: string;
@@ -13,9 +14,13 @@ class SindriService {
   constructor(noirService?: NoirService) {
     this.repository = new SindriRepository();
     this.noirService = noirService || new NoirService();
-    this.circuitId = process.env.EXPO_PUBLIC_CIRCUIT_ID || SindriService.DEFAULT_CIRCUIT_ID;
+    this.circuitId =
+      process.env.EXPO_PUBLIC_CIRCUIT_ID || SindriService.DEFAULT_CIRCUIT_ID;
   }
-  async pollForStatus(endpoint: string, timeout: number = SindriService.POLL_TIMEOUT_SECONDS) {
+  async pollForStatus(
+    endpoint: string,
+    timeout: number = SindriService.POLL_TIMEOUT_SECONDS
+  ) {
     for (let i = 0; i < timeout; i++) {
       const response = await this.repository.getRequest(endpoint);
       const status = response.data.status;
@@ -23,7 +28,9 @@ class SindriService {
         console.log(`Poll exited after ${i} seconds with status: ${status}`);
         return response;
       }
-      await new Promise((resolve) => setTimeout(resolve, SindriService.POLL_INTERVAL_MS));
+      await new Promise((resolve) =>
+        setTimeout(resolve, SindriService.POLL_INTERVAL_MS)
+      );
     }
     throw new Error(`Polling timed out after ${timeout} seconds.`);
   }
