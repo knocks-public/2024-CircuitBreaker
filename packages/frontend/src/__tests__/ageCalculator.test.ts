6d0cf67a39d496045e0d7bf7c145fb8dbfcae863
susumu tomita <oyster880@gmail.com>
2024-02-11 21:51:23 +0900

    add age calculator

--- packages/frontend/src/__tests__/ageCalculator.test.ts
+++ packages/frontend/src/__tests__/ageCalculator.test.ts
@@ -10,9 +10,12 @@ describe('calculateAge', () => {
   it('should account for today being before the birth date in "YYYYMMDD" format', () => {
     const today = new Date();
     const birthDate = `${today.getFullYear() - 25}${(today.getMonth() + 1).toString().padStart(2, '0')}${today.getDate().toString().padStart(2, '0')}`;
-    const expectedAge = today.getMonth() + 1 < parseInt(birthDate.substring(4, 6), 10) ||
-      (today.getMonth() + 1 === parseInt(birthDate.substring(4, 6), 10) && today.getDate() < parseInt(birthDate.substring(6, 8), 10))
-      ? 24 : 25;
+    const expectedAge =
+      today.getMonth() + 1 < parseInt(birthDate.substring(4, 6), 10) ||
+      (today.getMonth() + 1 === parseInt(birthDate.substring(4, 6), 10) &&
+        today.getDate() < parseInt(birthDate.substring(6, 8), 10))
+        ? 24
+        : 25;
     expect(calculateAge(birthDate)).toEqual(expectedAge);
   });
 });
